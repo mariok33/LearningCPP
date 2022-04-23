ad26f6ff69efafd2ae9b48ac13cd4906f7533604
Your Name <you@example.com>
2022-04-23 23:07:35 +0200

    implementation of CMakeLists.txt for Factorial and Palindrome subfolders

---
+++ Factorial/uts/factorialTests.cpp
@@ -0,0 +1,12 @@
+#include "factorial.hpp"
+#include "gtest/gtest.h"
+
+class FactorialTests : public ::testing::Test 
+{
+public:
+    FactorialTests() {};
+    ~FactorialTests() override {};
+
+    void SetUp() override {};
+    void TearDown() override {};
+};
---
+++ Palindrome/uts/palindromeTests.cpp
@@ -0,0 +1,13 @@
+#include "palindrome.hpp"
+
+#include "gtest/gtest.h"
+
+class PalindromeTests : public ::testing::Test 
+{
+public:
+    PalindromeTests() {};
+    ~PalindromeTests() override {};
+
+    void SetUp() override {};
+    void TearDown() override {};
+};
