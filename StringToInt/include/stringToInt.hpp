ce988dde483f2ef6fe2756421847e3ba30db31be
Your Name <you@example.com>
2022-04-27 13:14:46 +0200

    implement CMakeLists.txt for StringToInt project, add in main CMakeLists.txt performing tests for StringToInt project

---
+++ StringToInt/include/stringToInt.hpp
@@ -0,0 +1,11 @@
+// #pragma once
+
+// int changeStringToInt(std::string phrase)
+// {
+//     int returnedChar;
+//     for (const auto& c : phrase)
+//     {
+//         returnedChar = phrase[idx] - '0';
+//     }
+//     return returnedChar
+// }
---
+++ StringToInt/uts/stringToIntTests.cpp
@@ -0,0 +1,18 @@
+// #include "stringToInt.hpp"
+// #include "gtest/gtest.h"
+
+// class StringToIntTests : public ::testing::Test 
+// {
+// public:
+//     StringToIntTests() {};
+//     ~StringToIntTests() override {};
+
+//     void SetUp() override {};
+//     void TearDown() override {};
+// };
+
+// TEST_F(StringToIntTests, shouldReturn0ChangeStringToIntFunctionParameterValueOfA)
+// {
+//     std::string phrase {"A"};
+//     EXPECT_EQ(changeStringToInt(phrase), 17);
+// }
